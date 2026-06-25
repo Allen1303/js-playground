5eb000df358afccf6d7857d535cc9d4c43cada16
Allen <132642677+Allen1303@users.noreply.github.com>
2026-06-25 18:57:53 -0500

    Complete function method exercise using split(), join(), filter() etc..

---
+++ functional-programming/exercise22_functions.js
@@ -0,0 +1,21 @@
+"use strict";
+/**
+ @@param Apply Functional Programming to Convert Strings to URL Slugs*
+ @@param Task:
+ Fill in the urlSlug function so it converts a string title and returns the hyphenated version for the URL. 
+ You can use any of the methods covered in this section, and don't use replace. Here are the requirements:
+
+The input is a string with spaces and title-cased words
+The output is a string with the spaces between words replaced by a hyphen (-)
+The output should be all lower-cased letters
+The output should not have any spaces
+ * */
+function urlSlug(title) {
+  // Solution Code
+  return title
+    .split(" ")
+    .filter((str) => str !== "")
+    .join("-")
+    .toLowerCase();
+}
+console.log(urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone"));
