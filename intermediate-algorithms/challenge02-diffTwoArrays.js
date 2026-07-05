2f3447f9e800cc0ff4c595d31d3750834f6074cd
Allen <132642677+Allen1303@users.noreply.github.com>
2026-07-04 20:20:24 -0500

    Update challenges 2 and 3 with set object solution

--- intermediate-algorithms/challenge02-diffTwoArrays.js
+++ intermediate-algorithms/challenge02-diffTwoArrays.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 /**
  * @param : Diff Two Arrays
  * @Task: Compare two arrays and return a new array with any items only found in one of the two 
@@ -7,7 +7,10 @@
  * */
 function diffArray(arr1, arr2) {
   //solution using the filter method
-  const newArr = [...arr1.filter(item => !arr2.includes(item)), ...arr2.filter(item => !arr1.includes(item))];
+  const newArr = [
+    ...arr1.filter((item) => !arr2.includes(item)),
+    ...arr2.filter((item) => !arr1.includes(item)),
+  ];
   return newArr;
 }
 
@@ -15,7 +18,21 @@ console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
 //===============================================================================================================
 // Solution with concat method
 const diffArr = (arr1, arr2) => {
-  return arr1.concat(arr2).filter(item => !arr1.includes(item) || !arr2.includes(item))
-}
+  return arr1
+    .concat(arr2)
+    .filter((item) => !arr1.includes(item) || !arr2.includes(item));
+};
 
 console.log(diffArr([1, 2, 3, 4], [1, 2, 3, 4, 5]));
+//================================================================================================================
+//Solution using the Set() method
+const diffTwoArrays = (arr1, arr2) => {
+  const set1 = new Set(arr1),
+    set2 = new Set(arr2);
+  const uniqueArr = [
+    ...arr1.filter((item) => !set2.has(item)),
+    ...arr2.filter((item) => !set1.has(item)),
+  ];
+  return uniqueArr;
+};
+console.log(diffTwoArrays([2, 3, 4, 5], [2, 3, 4, 5, 6]));
--- intermediate-algorithms/challenge03-seekValues.js
+++ intermediate-algorithms/challenge03-seekValues.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 /**
 @param: * Seek and Destroy
 You will be provided with an initial array as the first argument to the destroyer function,
@@ -9,12 +9,21 @@ You can access the additional arguments by adding a rest parameter to the functi
 *
  * */
 const seekSameValues = (arr, ...rest) => {
-  return arr = [...arr.filter(value => !rest.includes(value)), ...rest.filter(value => !arr.includes(value))]
-}
+  return arr.filter((value) => !rest.includes(value));
+};
 console.log(seekSameValues([1, 2, 3, 1, 2, 3], 2, 3));
 //==============================================================================================================
 // Concat version
 const seekValues = (arr, ...rest) => {
-  return arr.concat(rest).filter(item => !arr.includes(item) || !rest.includes(item))
-}
-console.log(seekValues([2, 3, 4, 2, 3, 4], 3, 4))
+  return arr
+    .concat(rest)
+    .filter((item) => !arr.includes(item) || !rest.includes(item));
+};
+console.log(seekValues([2, 3, 4, 2, 3, 4], 3, 4));
+//=============================================================================================================
+//solution using Set, Set() are better when working with larger scale application of data.
+const removeSameValues = (arr, ...rest) => {
+  const uniqueSet = new Set(rest);
+  return arr.filter((element) => !uniqueSet.has(element));
+};
+console.log(removeSameValues([3, 4, 5, 3, 4, 5], 4, 5));
