1ac50e0773622c8e6ad496b7d5d01f68d2037f70
Allen <132642677+Allen1303@users.noreply.github.com>
2026-07-21 19:27:35 -0500

    Complete challenge with slice() & findIndex() to remove elements.

---
+++ intermediate-algorithms/challenge15-drop-array-element.js
@@ -0,0 +1,20 @@
+"use strict";
+/**
+ * @param Drop it
+Given the array arr, iterate through and remove each element starting from the first element (the 0 index) 
+until the function func returns true when the iterated element is passed through it.
+
+Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array.
+ *
+ * */
+function dropElements(arr, func) {
+  const arrItems = [...arr];
+  const itemIndex = arrItems.findIndex((element) => func(element));
+  return itemIndex === -1 ? [] : arrItems.slice(itemIndex);
+}
+
+console.log(
+  dropElements([1, 2, 3], function (n) {
+    return n < 3;
+  }),
+);
